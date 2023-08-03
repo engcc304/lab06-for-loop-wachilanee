d4e936de8a965e47f1eef42a625722b3c375f66f
wachilanee <wachilanee47@gmail.com>
2023-08-03 14:09:08 +0700

    111

--- Lab/Lab06-For-Loop.cpp
+++ Lab/Lab06-For-Loop.cpp
@@ -23,13 +23,13 @@ int main() {
     printf("Enter value : ");
     scanf("%d",&N);
     if ( N % 2 == 1){
-        printf("Series: ");
+        // printf("Series: ");
         for (i = 1 ; i <= N; i += 2){
         printf(" %d ",i);
     }
     }
     else{
-        printf("Series: ");
+        // printf("Series: ");
         for (i = N; i >= 0 ; i -= 2){
         printf(" %d ",i);
    }
